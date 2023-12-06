7821265ee219b4f7a7c56bf744cb227716429837
Surkhay <95585134+SurkhayFatullayev@users.noreply.github.com>
2023-12-06 21:16:17 +0400

    Add files via upload

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Assignment2rec";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "root";
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
